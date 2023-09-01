36bd04ef4fc4128c29fd82d7feba515efe8d26e2
WalterGuerra <wsteven00@gmail.com>
2023-09-01 14:16:15 -0400

    add object in constants for tetrominos for 7 shapes that have a shape property and color property

--- js/app.js
+++ js/app.js
@@ -1,6 +1,43 @@
 /*-------------------------------- Constants --------------------------------*/
 const rows = 20 
 const columns = 10
+const tetrominos = {
+  I: { 
+    shape: [[1,1,1,1]],
+    color: 'cyan'
+  },
+  J: {
+    shape: [[0,0,1],
+            [1,1,1],],
+    color: 'blue'
+  },
+  L: {
+    shape: [[1,0,0],
+            [1,1,1]],
+    color: 'orange'
+  },
+  O: {
+    shape: [[1,1],
+            [1,1]],
+    color: 'yellow'
+  },
+  S: {
+    shape: [[0,1,1],
+            [1,1,0]],
+    color: 'green'
+  },
+  T: {
+    shape: [[0,1,0],
+            [1,1,1]],
+    color: 'purple'
+  },
+  Z: {
+    shape: [[1,1,0],
+            [0,1,1]],
+    color: 'red'
+  },
+}
+console.log(tetrominos)
 /*---------------------------- Variables (state) ----------------------------*/
 
 
